8e3404fb0248910bdac930127c83128a48591773
ignacio-garza <ignacio.garza.s@outlook.com>
2023-02-19 22:57:52 -0600

    Added a second graph

--- master.r
+++ master.r
@@ -52,3 +52,25 @@ jpeg("other_pr.jpg", width = 900, height = 900, units = "px")
 graphy
 ignier()
 dev.off()
+
+
+### Second graph
+
+main |>
+    filter(Mes > as.yearmon("2021-12-31"),
+        months(Mes) == "enero") |>
+    mutate(Mes = as.character(Mes)) |>
+    ggplot(aes(x = Mes, y = Inflación, fill = Grupo_General,
+        group = NA, order = Grupo_General)) +
+    geom_dots(linewidth = 0) +
+    scale_y_continuous(limits = c(-0.3, 0.3), labels = scales::percent, n.breaks = 6) +
+    themo() +
+    theme(axis.title.y = element_text(hjust = .99, margin = margin(t = 0, r = 0, b = 0, l = 10)),
+            legend.position = c(.67, .23),
+              legend.background = element_blank(),
+              legend.direction = "vertical") +
+        labs(title = "Inflación de los genéricos por finalidades",
+        subtitle = "",
+        caption = "Fuente: INEGI \nNota:")
+
+### Third graph
